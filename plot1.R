6244529ecdddfc3bc5959d9c3c0de309b9cad9bd
gzyy1026 <zhenyu-guo@utulsa.edu>
2017-08-27 22:39:04 -0500

    first

---
+++ plot1.R
@@ -0,0 +1,12 @@
+library(datasets)
+
+household_power<-read.table("household_power_consumption.txt",sep=";",nrows=2075259,header=TRUE,stringsAsFactors=FALSE)
+
+
+subs<-subset(household_power,Date=="2/1/2007"|Date=="2/2/2007")
+
+gap<-as.numeric(subs$Global_active_power)
+
+hist(gap,xlab="Global Active Power (kilowatts)",ylab="Frequency", col="red",main="Global Active Power")
+dev.copy(png, "plot1.png")
+dev.off()
